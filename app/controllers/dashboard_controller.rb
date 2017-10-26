da4e3c554dfd334fa094cd30fcf20242731b8b9a
Mashrur Hossain <tddror5railsudemy@gmail.com>
2017-10-26 10:53:30 +0000

    Restrict access for non logged in users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!, except: [:front]
   
   def front
-    
+    redirect_to home_path if current_user
   end
   
   def home
---
+++ spec/requests/dashboard_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "Dashboard", type: :request do
+  describe 'Get /' do
+    it 'displays the home page' do
+      get "/"
+      
+      expect(response.status).to eq 200
+    end
+    
+    it "redirects to /home if signed in" do
+      john = User.create(email: "john@example.com", password: "password")
+      login_as(john)
+      
+      get "/"
+      expect(response.status).to eq 302
+      expect(response).to redirect_to(home_path)
+      
+    end
+  end
+  
+  describe 'Get /home' do
+    let(:john) { User.create(email: "john@example.com", password: "password")}
+    
+    context 'with unauthenticated user' do
+      before do
+        get "/home"
+      end
+      
+      it "redirects to the sign up path" do
+        expect(response.status).to eq 302
+        expect(response).to redirect_to(user_session_path)
+      end
+    end
+    
+    context 'with authenticated user' do
+      before do
+        login_as(john)
+        get "/home"
+      end
+      
+      it "successfully accesses the home page" do
+        expect(response.status).to eq 200
+      end
+    end
+    
+  end
+end
